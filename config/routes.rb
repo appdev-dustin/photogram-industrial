f2800bb6a064240216bf090c1affaea2f44a43be
sgnlcntxt <dieckmann.dustin@gmail.com>
2022-04-19 00:46:16 +0000

    Done with feed

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,13 @@ Rails.application.routes.draw do
   resources :comments
   resources :photos
 
-  get "/:username" => "users#show"
+
+  get ":username/liked" => "photos#liked", as: :liked_photo
+  get ":username/feed" => "photos#feed", as: :feed_photo
+  get ":username/followers" => "follow_requests#followers", as: :followers
+  get ":username/following" => "follow_requests#following", as: :following
+
+  get ":username" => "users#show", as: :user
   
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
